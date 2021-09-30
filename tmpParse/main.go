9ce554899dedca14146fb3e2b713cf5b56dce9c2
gosilviu <silviu.accy@yahoo.com>
2021-10-01 00:08:47 +0300

    registerauthhandler

--- tmpParse/main.go
+++ tmpParse/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
 	"text/template"
+	"unicode"
+
+	"golang.org/x/crypto/bcrypt"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -15,13 +19,18 @@ func main() {
 
 	tpl, _ = template.ParseGlob("templates/*.html")
 	var err error
-	db, err = sql.Open("mysql", "root:password@tcp(localhost:3306)/mygodatabase")
+	db, err = sql.Open("mysql", "root:Federerthebestcasa2007!!!@tcp(127.0.0.1:3306)/mygodatabase")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/contact", contactHandler)
 	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/register", registerHandler)
+	http.HandleFunc("/registerauth", registerAuthHandler)
 	http.ListenAndServe(":8080", nil)
 }
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,4 +47,88 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 }
 func registerHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "register.html", nil)
+}
+func registerAuthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("*****registerAuthHandler running*****")
+	r.ParseForm()
+	username := r.FormValue("username")
+	var nameAlphaNumeric = true
+	for _, char := range username {
+		if unicode.IsLetter(char) == false && unicode.IsNumber(char) == false {
+			nameAlphaNumeric = false
+		}
+	}
+	var nameLength bool
+	if 5 <= len(username) && len(username) <= 50 {
+		nameLength = true
+	}
+	password := r.FormValue("password")
+	fmt.Println("password:", password, "\npswdLength:", len(password))
+	var pswdLowercase, pswdUppercase, pswdNumber, pswdSpecial, pswdLength, pswdNoSpaces bool
+	pswdNoSpaces = true
+	for _, char := range password {
+		switch {
+		case unicode.IsLower(char):
+			pswdLowercase = true
+		case unicode.IsUpper(char):
+			pswdUppercase = true
+		case unicode.IsNumber(char):
+			pswdNumber = true
+		case unicode.IsPunct(char) || unicode.IsSymbol(char):
+			pswdSpecial = true
+		case unicode.IsSpace(int32(char)):
+			pswdNoSpaces = false
+		}
+	}
+	if 11 < len(password) && len(password) < 60 {
+		pswdLength = true
+	}
+	fmt.Println("pswdLowercase:", pswdLowercase, "\npswdUppercase:", pswdUppercase, "\npswdNumber:", pswdNumber, "\npswdSpecial:", pswdSpecial, "\npswdLength:", pswdLength, "\npswdNoSpaces:", pswdNoSpaces, "\nnameAlphaNumeric:", nameAlphaNumeric, "\nnameLength:", nameLength)
+	if !pswdLowercase || !pswdLength || !pswdNoSpaces || !pswdNumber || !pswdUppercase || !pswdSpecial || !nameAlphaNumeric || !nameLength {
+		tpl.ExecuteTemplate(w, "register.html", "please check username and password criteria")
+		return
+	}
+	stmt := "SELECT id FROM register WHERE user = ?"
+	row := db.QueryRow(stmt, username)
+	var uID string
+	err := row.Scan(&uID)
+	if err != sql.ErrNoRows {
+		fmt.Println("username aleready exists, err:", err)
+		tpl.ExecuteTemplate(w, "register.html", "username already taken")
+		return
+	}
+	var hash []byte
+	hash, err = bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Println("bcrypt err:", err)
+		tpl.ExecuteTemplate(w, "register.html", "there was a problem resiterging account")
+		return
+	}
+	fmt.Println("hash:", hash)
+	fmt.Println("string(hash):", string(hash))
+	var insertStmt *sql.Stmt
+	insertStmt, err = db.Prepare("INSERT INTO register (user, pass) VALUES (?, ?);")
+	if err != nil {
+		fmt.Println("error preparing statement:", err)
+		tpl.ExecuteTemplate(w, "register.html", "there was a problem registering account")
+		return
+	}
+	defer insertStmt.Close()
+	fmt.Println("--------------------")
+	var result sql.Result
+
+	result, err = insertStmt.Exec(username, hash)
+	rowsAff, _ := result.RowsAffected()
+	fmt.Println("--------------------")
+	lastIns, _ := result.LastInsertId()
+
+	fmt.Println("rowsAff:", rowsAff)
+	fmt.Println("lastIns:", lastIns)
+	fmt.Println("err:", err)
+	if err != nil {
+		fmt.Println("error inserting new user")
+		tpl.ExecuteTemplate(w, "register.html", "there was a problem registering account")
+		return
+	}
+	fmt.Fprint(w, "congrats, your account has been successfully created")
 }
